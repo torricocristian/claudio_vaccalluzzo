8173c58aab20796e8d0c28a54ae4ef46bf52fc46
facu-ro <facu_jaja@hotmail.com>
2022-12-10 20:18:22 -0300

    p

--- claudio.php
+++ claudio.php
@@ -31,139 +31,7 @@
     </div>
 
 
-    <div class="gallery" id="galeria">
-
-        <!--
-        <a href="image/img_claudio/1.jpg" rel="lightbox" data-lightbox="roadtrip"> 
-             
-            <figure>
-            
-                <img src="image/img_claudio/1.jpg" alt="foto_claudio_vacalluzzo" rel="lightbox" data-lightbox="roadtrip"> 
-                    
-            </figure>
-        </a>
-
-
-        <figure>
-            <img src="image/img_claudio/2.jpg" alt="foto_claudio_vacalluzzo">
-        </figure>
-
-        <figure>
-            <img src="image/img_claudio/3.jpg" alt="foto_claudio_vacalluzzo">
-        </figure>
-
-        <figure>
-            <img src="image/img_claudio/4.jpg" alt="foto_claudio_vacalluzzo">
-        </figure>
-
-        <figure>
-            <img src="image/img_claudio/5.jpg" alt="foto_claudio_vacalluzzo">
-        </figure>
-
-        <figure>
-            <img src="image/img_claudio/6.jpg" alt="foto_claudio_vacalluzzo">
-        </figure>
-
-        <figure>
-            <img src="image/img_claudio/7.jpg" alt="foto_claudio_vacalluzzo">
-        </figure>
-
-        <figure>
-            <img src="image/img_claudio/8.jpg" alt="foto_claudio_vacalluzzo">
-        </figure>
-
-        <figure>
-            <img src="image/img_claudio/9.jpg" alt="foto_claudio_vacalluzzo">
-        </figure>
-      
-        <figure>
-            <img src="image/img_claudio/10.jpg" alt="foto_claudio_vacalluzzo">
-        </figure>
-        
-
-        <img src="image/img_claudio/1.jpg"  alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/2.jpg"  alt="foto_claudio_vacalluzzo">
-       
-        <img src="image/img_claudio/3.jpg"  alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/4.jpg"  alt="foto_claudio_vacalluzzo">
-       
-        <img src="image/img_claudio/5.jpg"  alt="foto_claudio_vacalluzzo">
-       
-        <img src="image/img_claudio/6.jpg"  alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/7.jpg"  alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/8.jpg"  alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/9.jpg"  alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/10.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/11.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/12.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/13.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/14.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/15.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/16.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/17.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/18.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/19.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/20.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/21.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/22.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/23.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/24.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/25.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/26.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/27.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/28.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/29.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/30.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/31.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/32.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/33.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/34.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/35.jpg" alt="foto_claudio_vacalluzzo">
-            
-        <img src="image/img_claudio/36.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/37.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/38.jpg" alt="foto_claudio_vacalluzzo">
-            
-        <img src="image/img_claudio/39.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/40.jpg" alt="foto_claudio_vacalluzzo">
-        
-        <img src="image/img_claudio/41.jpg" alt="foto_claudio_vacalluzzo">
-    -->
-    </div>
+    <div class="gallery" id="galeria"> </div>
 
     
 
@@ -221,7 +89,7 @@
         
         <h3>Comentarios</h3>
 
-        <div class="fb-comments" data-href="http://www.hdosso.com.ar/" data-width="100%" data-numposts="6"></div>
+        <div class="fb-comments" data-href="https://buenviaje2.dispenserjg.com/claudio.php" data-width="100%" data-numposts="6"></div>
 
     
     </section>
